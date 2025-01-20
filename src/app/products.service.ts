7997ad218ab45ba2c8d61f7d78400e56733585b8
JimwellC <Jimwellwork28@gmail.com>
2025-01-20 13:04:43 +0800

    fourths commit

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -9,7 +9,7 @@ export class ProductsService {
     { id: "P-102", name: 'JBL BT Speaker', description: 'Waterproof Radio 360 Surround.', price: 1099 },
     { id: "P-103", name: 'Mechanical Keyboard', description: 'Hot-Swappable RGB Backlit.', price: 2395 },
     { id: "P-104", name: 'Oculus Meta', description: 'All-i-one Gaming Headset.', price: 22450 },
-    { id: "P-105", name: 'Jimwell Calma', description: 'Tall Dark and Handsome!!!.', price: 1000000 }
+    { id: "P-105", name: 'Jimwell Calma', description: 'Tall Dark and Handsome!!!!.', price: 1000000 }
   ];
 
   getProducts() {
