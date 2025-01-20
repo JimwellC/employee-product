c1141b8d7ed7910d42905b5c6d13a3bc5b00d311
Rakishi pogi <rakishiduelle@gmail.com>
2025-01-20 12:33:34 +0800

    initial input

---
+++ src/app/app.component.ts
@@ -0,0 +1,56 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { EmployeeService } from './employee.service';
+import { ProductsService } from './products.service';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'services-demo';
+  public employees: {
+    id: number;
+    firstname: string;
+    lastname: string;
+    email: string;
+  }[] = [];
+  products: any[] = [];
+
+  constructor(private _employeeService: EmployeeService, private productsService: ProductsService) { }
+
+  ngOnInit() {
+    this.employees = this._employeeService.getEmployees();
+    this.products = this.productsService.getProducts();
+  }
+//   public employees = [
+//   {
+//     id: 101,
+//     firstname: 'Joseph',
+//     lastname: 'Dizon',
+//     email: 'jdizon@hau.edu.ph'
+//   },
+//   {
+//     id: 102,
+//     firstname: 'James',
+//     lastname: 'Atienza',
+//     email: 'jatienza@hau.edu.ph',
+//   },
+//   {
+//     id: 103,
+//     firstname: 'John',
+//     lastname: 'Cena',
+//     email: 'jcena@hau.edu.ph',
+//   },
+//   {
+//     id: 104,
+//     firstname: 'Robert',
+//     lastname: 'Quintana',
+//     email: 'rquintana@hau.edu.ph',
+//   },
+// ];
+}
